20d43b48ae464c254fc5bdcad3f243d3a2244b0a
Reid Taylor <rebridgers@gmail.com>
2026-06-05 11:50:37 -0500

    Implement jacobian descent for pre-training

--- src/json2vec/architecture/runtime.py
+++ src/json2vec/architecture/runtime.py
@@ -35,6 +35,8 @@ if TYPE_CHECKING:
 class Output(TypedDict):
     loss: NotRequired[torch.Tensor]
     predictions: NotRequired[list[Prediction]]
+    losses: NotRequired[list[torch.Tensor]]
+    addresses: NotRequired[list[Address]]
 
 
 Preprocessor: TypeAlias = Callable[[dict[str, Any]], dict[str, Any]]
@@ -140,6 +142,7 @@ class ModelRuntime:
             return Output(predictions=predictions)
 
         losses: list[torch.Tensor] = []
+        addresses: list[Address] = []
 
         for prediction in predictions:
             if prediction.address not in module.schema.requests:
@@ -155,11 +158,18 @@ class ModelRuntime:
 
             loss: torch.Tensor = loss_fn(module=module, prediction=prediction, batch=batch[address], strata=strata)
             losses.append(loss * torch.tensor(request.weight))
+            addresses.append(address)
 
         if len(losses) == 0:
             logger.warning("no trainable fields in batch, returning zero loss")
             loss: torch.Tensor = torch.tensor(0.0, device=batch.device, requires_grad=True)
             return Output(loss=loss)
+        
+        if strata==Strata.train:
+            return Output(
+                losses=losses,
+                addresses=addresses
+            )
 
         loss: torch.Tensor = module.track((Metric.loss, strata), value=torch.stack(losses).sum())
         return Output(loss=loss)
